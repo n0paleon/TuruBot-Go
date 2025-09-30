7512d24d3ac1df1431738c022603b0d2b7fa150f
Muhammad Naufal Al Fattah <muhammadnaufalalfattah@gmail.com>
2025-10-01 02:42:04 +0700

    add automattic env support from os

--- internal/config/viper.go
+++ internal/config/viper.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 
 	"github.com/spf13/viper"
 )
@@ -21,7 +22,7 @@ func LoadConfig(filepath string) (*Config, error) {
 	v.SetDefault("DB_DSN", "file:session_store.db?_foreign_keys=on")
 
 	if err := v.ReadInConfig(); err != nil {
-		return nil, fmt.Errorf("failed to read config file: %w", err)
+		logrus.Errorf("failed to read config file: %w", err)
 	}
 
 	cfg := &Config{
